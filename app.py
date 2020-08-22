a01d084eda34cddcca3c286e217d9972ee9fe94d
Michael Berry <66836804+michaelthomasberry@users.noreply.github.com>
2020-08-22 20:19:59 +0100

    Add files via upload
    added the app.py and index to GitHub

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    ## to change the port in flask you have to type: export FLASK_APP=application.py
